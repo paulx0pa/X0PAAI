71dc4b0f8d795b4187602551059d0a232555beec
paulx0pa <114465744+paulx0pa@users.noreply.github.com>
2022-10-18 21:02:25 +0530

    Update Loginlogout.java

--- src/test/java/testrunners/Loginlogout.java
+++ src/test/java/testrunners/Loginlogout.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features="Featurefiles/loginlogout.feature",glue="stepdefinitions",dryRun=false,
 plugin={"com.cucumber.listener.ExtentCucumberFormatter:Reports/loginlogout.html"})
 public class Loginlogout extends AbstractTestNGCucumberTests{
